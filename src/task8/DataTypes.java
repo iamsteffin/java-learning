ca80bd3067eadb213a910a9578c5f503b25dd79a
steffin <steffin@gmail.com>
2025-10-13 12:38:23 -0500

    Primitive Data Types

---
+++ src/task8/DataTypes.java
@@ -0,0 +1,25 @@
+package task8;
+//Declare and initialize all primitive data types and print their values
+
+public class DataTypes {
+    public static void main(String[] args){
+        boolean bool = true;
+        byte b = 100;
+        char A = 'a';
+        short sh = 15000;
+        int i = 15000000;
+        long l = 150000000000000L;
+        float f = 10.78f;
+        double d = 17.258963;
+
+        System.out.println("boolean value: " + bool);
+        System.out.println("byte value: " + b);
+        System.out.println("char value: " + A);
+        System.out.println("short value: " + sh);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+
+    }
+}
